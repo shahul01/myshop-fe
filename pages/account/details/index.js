3a16f51f4f3e182218556b4460035d1a4cde7efb
shahul01 <shahulhammeed01@gmail.com>
2021-10-09 14:28:05 +0530

    feat: created basic layout for account details

---
+++ pages/account/details/index.js
@@ -0,0 +1,20 @@
+// import styles from "./accdetails.modules.css";
+
+const AccDetails = () => {
+
+
+  return (
+    <div>
+
+      <h2>Address Form</h2>
+      {/* className="address-form-container" */}
+      <div>
+
+      </div>
+
+
+    </div>
+  )
+}
+
+export default AccDetails;
--- pages/account/index.js
+++ pages/account/index.js
@@ -1,14 +1,28 @@
 import { useRouter } from "next/router";
+import { useContext } from "react";
+import { UserContext } from "helpers/Contexts/UserContext";
 
 
 const Account = () => {
 
   const router = useRouter();
+  const { user, dispatch } = useContext(UserContext);
 
+  // COMMT: TODO: make the following push work
+  // if (!user.isUserSignedIn) () => return router.push('/account/validation');
   return (
     <>
-      <button className="button" onClick={() => router.push('/account/validation')}>
-        Go to account validation
+      <button
+        className="button"
+        onClick={() => router.push('/account/validation')}
+      >
+        Go to Registration / Sign in page
+      </button>
+      <button
+        className="button"
+        onClick={() => router.push('/account/details')}
+      >
+        Go to Account details
       </button>
     </>
   )
