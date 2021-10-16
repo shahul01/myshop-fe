0e050130f9297c9f3fe675c642471e506dafb2f3
shahul01 <shahulhammeed01@gmail.com>
2021-10-16 18:17:18 +0530

    refactor: cleanedup

--- pages/account/details/index.js
+++ pages/account/details/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "helpers/Contexts/UserContext";
-import getLocalUser from "helpers/Functions/getLocalUser";
 import Input from "components/Elements/Input/index";
 import { getAddress, postAddress } from "./_api/detailsApi";
 // import styles from "./accdetails.modules.css";
@@ -8,14 +7,9 @@ import { getAddress, postAddress } from "./_api/detailsApi";
 
 const AccDetails = () => {
 
-
-  const { user, dispatch } = useContext(UserContext);
-
-  let userAxiosGet = {};
+  const { user } = useContext(UserContext);
   const [isPut, setIsPut] = useState(false);
-  const [ userState, setUserState ] = useState({});
   const [ isLoadDom, setIsLoadDom ] = useState(false);
-  const [ userAxiosPost, setUserAxiosPost ] = useState({});
   const [ addressForm, setAddressForm ] = useState({
     street: '',
     city: '',
@@ -25,80 +19,23 @@ const AccDetails = () => {
     fullName: '',
     phoneNumber: '',
   });
-  let alertSetUserAxiosPost = {};
-  // let tempGetUser = '';
-  // let tempLoadAddress = '';
-
-  useEffect( async () => {
-    let tempUserFetch = getLocalUser();
-    if (tempUserFetch) {
-      await setUserAxiosPost(tempUserFetch)
-    }
-    // const tempUser = await getLocalUser();
-    // if (tempUser) {
-    //   alertSetUserAxiosPost = tempUser
-    // };
-    await getUser();
-    await loadAddress();
-    // console.log(`alertSetUserAxiosPost 0: `, alertSetUserAxiosPost);
-    // console.log(`userAxiosGet: `, userAxiosGet);
-    // console.log(`userAxiosPost: `, userAxiosPost);
-  }, []);
-
-  // useEffect(() => {
-  //   // setUserAxiosPost(alertSetUserAxiosPost);
-  //   // console.log(`alertSetUserAxiosPost: `, alertSetUserAxiosPost);
-  // }, [alertSetUserAxiosPost])
-  async function getUser() {
-    userAxiosGet = await getLocalUser();
-    // console.log(`userAxiosGet: `, userAxiosGet);
-    return userAxiosGet;
-  };
 
   useEffect(() => {
-    // if (typeof window !== undefined) {
-      // setUserState(user);
-      // console.log(`userState: `, userState);
-      // console.log(`userState: `, userState);
-
-      console.log(`user: `, user);
-      setTimeout(() => {
-        loadAddress();
-
-      }, 4000)
-      // if(user.username !== '') {
-      //   setIsLoadDom(true);
-      // }
-      console.log(`isLoadDom: `, isLoadDom);
-    // };
+    loadAddress();
   }, [user])
 
-
   async function loadAddress() {
-    // console.log(`userAxiosGet: `, userAxiosGet);
-    // console.log(`userAxiosPost: `, userAxiosPost);
-    console.log(`user: `, user);
-
-
-    // if(user.username !== '') {
-    //   setIsLoadDom(true);
-    // };
-    // if(!userAxiosGet?.user?.id) return;
+    setIsLoadDom(false);
     if(!user?.userId) return;
     let myAdr = await getAddress(user?.userId);
-    // let myAdr = await getAddress(userAxiosPost?.user?.id);
     if (myAdr.length >= 1) {
       setAddressForm(myAdr[0]);
       setIsPut(true);
     };
     setIsLoadDom(true);
-    console.log(`addressForm: `, addressForm);
-    console.log('loadAddress runs');
   };
 
   function handleChange(e) {
-    // if (e?.target?.value) return;
-
     setAddressForm({
       ...addressForm,
       [e.target.name]: e.target.value
@@ -107,13 +44,11 @@ const AccDetails = () => {
 
   async function handleSubmit(e, submittedForm) {
     e.preventDefault();
-    // if (!submittedForm.fullName) return
+    if (!submittedForm.fullName || !user?.userId) return
     const toSubmitForm = {
       ...submittedForm,
-      users_permissions_user: userAxiosPost?.user?.id
+      users_permissions_user: user.userId
     };
-    // console.log(`userAxiosPost: `, userAxiosPost);
-    // const resPost = await postAddress(submittedForm);
     if(!isPut) {
       const resPost = await postAddress(toSubmitForm);
       console.log(`resPost: `, resPost);
